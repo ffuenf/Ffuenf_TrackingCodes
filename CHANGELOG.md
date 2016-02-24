1d1860e35a1b9f414ef101d7127bdab17addf334
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2016-02-24 18:22:54 +0100

    initial public release add Facebook Pixel

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for Ffuenf_TrackingCodes
+
+This file is used to list changes made in each version of Ffuenf_TrackingCodes.
+
+## 2.0.0 (February 24, 2015)
+
+* initial public release
+* add Facebook Pixel
+
+## 1.1.0 (December 3, 2015)
+
+* initial internal release
---
+++ app/code/local/Ffuenf/TrackingCodes/Test/Helper/Data.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Ffuenf_TrackingCodes extension.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category   Ffuenf
+ *
+ * @author     Achim Rosenhagen <a.rosenhagen@ffuenf.de>
+ * @copyright  Copyright (c) 2016 ffuenf (http://www.ffuenf.de)
+ * @license    http://opensource.org/licenses/mit-license.php MIT License
+ */
+
+/**
+ * @see Ffuenf_TrackingCodes_Helper_Data
+ *
+ * @loadSharedFixture shared
+ */
+
+class Ffuenf_TrackingCodes_Test_Helper_Data extends EcomDev_PHPUnit_Test_Case
+{
+    /**
+     * @var Ffuenf_DevTools_Test_Helper_Data
+     */
+    protected $_helper;
+
+    public function setUp()
+    {
+        $this->_helper = new Ffuenf_TrackingCodes_Helper_Data();
+    }
+
+    public function tearDown()
+    {
+        $this->helper = null;
+    }
+
+    /**
+     * Tests whether extension is active.
+     *
+     * @test
+     * @covers Ffuenf_DevTools_Helper_Data::isExtensionActive
+     */
+    public function testIsExtensionActive()
+    {
+        $this->assertTrue(
+            $this->_helper->isExtensionActive(),
+            'Extension is not active please check config'
+        );
+    }
+}
